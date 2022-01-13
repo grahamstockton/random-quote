451623bb04334f37803c7e1232a4b1f6e856a9f4
Graham Stockton <graham.stockton@utexas.edu>
2022-01-13 11:36:11 -0600

    Initial commit
    Trying to deploy. Never done this with gh-pages before.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { getQuotes } from './quotes';
+import { useState } from 'react';
 
+// Declare this file after quotes in order to get access to the quotes variab
+
+// quotes variable
+var quotes = getQuotes()['quotes'];
+
+// returns a quote at random from quotes
+function getNewQuote() {
+  var quoteKeys = Object.keys(quotes);
+  return quotes[quoteKeys[ Math.floor(quoteKeys.length * Math.random()) ]];
+}
+
+// display -- quote changes when button is clicked. has a tweet link
 function App() {
+  const [state, setState] = useState(getNewQuote());
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="center" id="quote-box">
+        <h2 id="text">{state.quote}</h2>
+        <div id="controls">
+          <p id="author">{state.author}</p>
+          <button className='btn push' id="new-quote" onClick={() => setState(getNewQuote)}>New Quote</button>
+          <a id="tweet-quote" href={'https://twitter.com/intent/tweet?text=' + encodeURIComponent('"' + state.quote + '" -' + state.author)}>Tweet</a>
+        </div>
+      </div>
     </div>
   );
 }
